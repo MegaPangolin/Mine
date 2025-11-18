d123839f95b47f5ba30aed94cdf89d9ec6736bda
Brandon <bestr019@jaguar.tamu.edu>
2025-11-17 19:55:49 -0600

    banana

--- arrays.java
+++ arrays.java
@@ -1,1 +1,15 @@
+Integer[][] raggedIntegerArray = {
+    {1, 2, null, 3},
+    {4, null, 5}
+};
 
+for (int i = 0; i < raggedIntegerArray.length; i++) {
+    for (int j = 0; j < raggedIntegerArray[i].length; j++) {
+        if (raggedIntegerArray[i][j] == null) {
+            System.out.print("  ");
+        } else {
+            System.out.print(raggedIntegerArray[i][j] + " ");
+        }
+    }
+    System.out.println();
+}
