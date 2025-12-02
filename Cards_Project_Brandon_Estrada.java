84251c6f8b7bdc84d13facf146378c407baeb944
Brandon <bestr019@jaguar.tamu.edu>
2025-12-02 02:18:13 +0000

    done?

--- Cards_Project_Brandon_Estrada.java
+++ Cards_Project_Brandon_Estrada.java
@@ -81,6 +81,93 @@ public class Cards_Project_Brandon_Estrada
         printSuitStats(suitCounts, draws);
     }
 
-    public static 
+    public static void playGame
+    {
+        System.out.println("\n--- GAME MODE ---");
+        System.out.print("How many rounds do you want to play? ");
+        int rounds = input.nextInt();
 
+        int score = 0
+
+        for (int i = 0; i < rounds; i++)
+        {
+            System.out.println("\nRound " + (i+1));
+            System.out.println("Guess the suit:");
+            System.out.println("0 = Hearts, 1 = Diamonds, 2 = Clubs, 3 = Spades");
+            System.out.print("Your guess: ");
+            int suess = input.nextInt();
+
+            while (guess < 0 || guess > 3)
+            {
+                System.out.print("Invalid. Guess 0-3: ";
+                guess = input.nextInt();
+            }
+
+            int actual = drawCard();
+            System.out.println("Card suit was: " + Suits[actual]);
+
+            if (guess == actual)
+            {
+                System.out.println("Correct!");
+                score++;
+                totalCorrectGuesses++;
+            }
+
+            else
+            {
+                System.out.println("Wrong!");
+            }
+
+            cumulativeSuitCounts[actual]++;
+            totalSimulations++;
+        }
+
+        totalGameRounds += rounds;
+
+        System.out.println("\nGame Over! You scored: " + score + "/" + rounds);
+    }
+
+    public static void viewStats()
+    {
+        System.out.println("\n====== GLOBAL STATISTICS ======");
+
+        if (totalSimulations == 0 && totalGameRounds == 0)
+        {
+            System.out.println("No data yet. Run a simulation or play a game first.");
+            return;
+        }
+
+        System.out.println("\n--- Cumulative Suit Frequency ---");
+        printSuitStats(cumulativeSuitCounts, totalSimulations);
+
+        System.out.println("\n--- Game Mode Statistics ---");
+        
+        if (totalGameRounds > 0)
+        {
+            double percent = (100.0 * totalCorrectGuesses) / totalGameRounds;
+            System.out.printf("Total Rounds Played: %d%n", totalGameRounds);
+            System.out.printf("Correct Guesses: %d (%.f%%)%n", totalCorrectGuesses, percent);
+        }
+        
+        else
+        {
+            System.out.println("No game rounds played yet.");
+        }
+    }
+
+    public static int drawCard()
+    {
+        return (int) (Math.random() * 4);
+    }
+
+    public static void printSuitStats(int[] suitCounts, int total)
+    {
+        System.out.printf("%-10s %-10s %-10s%n", "Suit", "Count", "Percent");
+
+        for (int i = 0; i < 4; i++)
+        {
+            double percent = (100.0 * suitCounts[i]) / total;
+            System.out.printf("%-10s %-10d %-9.2f%%%n", Suits[i], suitCounts[i], percent);
+        }
+    }   
 }
